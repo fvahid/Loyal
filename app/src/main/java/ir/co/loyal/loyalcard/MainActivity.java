a3a8072437d9c2af80ecbc292e9ad0d715a44c2b
vfardi <vahidfardi2011@gmail.com>
2017-05-26 20:56:30 +0430

    add setting menu with some bug

--- app/src/main/java/ir/co/loyal/loyalcard/MainActivity.java
+++ app/src/main/java/ir/co/loyal/loyalcard/MainActivity.java
@@ -1,5 +1,6 @@
 package ir.co.loyal.loyalcard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import ir.co.loyal.loyalcard.Setting;
+
+import static android.R.attr.value;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
+            //return true;
+            Intent intent = new Intent(getApplicationContext(), Setting.class);
+            startActivity(intent);
             return true;
         }
 
